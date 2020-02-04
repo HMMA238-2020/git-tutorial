47fb5f2703224d6fcd74eb688f967d3d18ef3032
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2020-02-04 22:59:10 +0100

    Data generation is no longer Gaussian!

--- generate_data.py
+++ generate_data.py
@@ -3,7 +3,7 @@ import numpy as np
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
-mu, sigma = 100, 15
-x = mu + sigma * np.random.randn(10000)
+mu, sigma = 100, 5
+x = mu + sigma * (np.random.randn(10,10000) ** 2).sum(0)
 
 np.savetxt('file_data.csv', x, delimiter=',')
