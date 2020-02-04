6be32404ea74120b25a258048f13b0b7056463fb
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2020-02-04 22:42:10 +0100

    Change opacity in the plot

--- plot.py
+++ plot.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 x = np.loadtxt('file_data.csv')
 
 # the histogram of the data
-n, bins, patches = plt.hist(x, 50, density=True, facecolor='r, alpha=0.75)
+n, bins, patches = plt.hist(x, 50, density=True, facecolor='r, alpha=0.74)
 
-plt.xlabel('Smarts')
-plt.ylabel('Probability')
-plt.title('Histogram of IQ')
-plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
-plt.xlim(40, 160)
-plt.ylim(0, 0.03)
-plt.grid(True)
-plt.show()
+ plt.xlabel('Smarts')
+ plt.ylabel('Probability')
+ plt.title('Histogram of IQ')
+ plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+ plt.xlim(40, 160)
+ plt.ylim(0, 0.03)
+ plt.grid(True)
+ plt.show()
